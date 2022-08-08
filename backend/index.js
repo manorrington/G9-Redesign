9de8a200b1b6673d4f955a3f1813ae9e786d005b
Micah Norrington <manorrington@gmail.com>
2022-08-08 15:51:29 -0400

    DB connection + middleware imports

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,25 @@
-const express = require('express')
-const app = express()
+// imports
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const app = express();
 
+// database connection
+mongoose.connect(
+     "mongodb+srv://dbUSER:dbUSER@redesigndb.aj1vcme.mongodb.net/?retryWrites=true&w=majority", 
+      { useNewUrlParser: true, 
+      },
+      () => console.log("MongoDB connection successful!")
+    );
+
+// second database ( if necessary )
+
+// middleware
+app.use(bodyparser.json());
+app.use(cors());
+
+// ----------------------------------- endpoints -------------------------- //
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
