5803378ad93ad1c940a9e24e4921729bd8ae1ae1
Micah Norrington <manorrington@gmail.com>
2022-08-08 16:09:25 -0400

    database connection successful

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,23 @@ const app = express();
 
 // database connection
 mongoose.connect(
-     "mongodb+srv://dbUSER:dbUSER@redesigndb.aj1vcme.mongodb.net/?retryWrites=true&w=majority", 
-      { useNewUrlParser: true, 
-      },
-      () => console.log("MongoDB connection successful!")
-    );
+  "mongodb+srv://dbUSER:dbUSER@redesigndb.aj1vcme.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => console.log("MongoDB connection successful!")
+);
+
+// create DB schema
+const redesignS = new mongoose.Schema({
+  item: String,
+});
+
+const Stuff = mongoose.model("Stuff", redesignS);
+const itemOne = Stuff({
+  item: "testing"
+}).save(function (err) {
+  if (err) throw err;
+  console.log("item saved");
+});
 
 // second database ( if necessary )
 
@@ -20,11 +32,11 @@ app.use(bodyparser.json());
 app.use(cors());
 
 // ----------------------------------- endpoints -------------------------- //
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 // port
 app.listen(3001, () => {
-    console.log("listening on 3001");
-  });
+  console.log("listening on 3001");
+});
