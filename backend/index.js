a369406bed6fc7057e2efeb26c0158a7fc7ce2b3
Micah Norrington <manorrington@gmail.com>
2022-08-09 13:35:09 -0400

    backend changes added

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const Test = require("./models/test");
+const Test = require("models/test");
 const app = express();
 
 // database connection
