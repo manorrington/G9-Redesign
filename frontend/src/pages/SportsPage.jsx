9ce0ed8e29b250b6c390d3cfddcded8149a8a1d6
Tyali Warren <tyaliwarren@MacBook-Pro-3.lan>
2022-08-10 19:59:28 -0400

    more

--- frontend/src/pages/SportsPage.jsx
+++ frontend/src/pages/SportsPage.jsx
@@ -1,18 +1,87 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import YouTube from "react-youtube";
 import './sports.css';
 
 const SportsPage = () => {
-    return ( 
-
+     const opts = {
+         height: "350",
+         width: "640",
+         playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+         autoplay: 1
+  }
+};
+  return (
+    
     <div>
         <Navbar />
+        <br />
     <section class="section">  
-      <div class = "box-main">
-        <div class="firstHalf">
+      <div className = "box-main">
+        <div className="firstHalf">
             <h1 class="text-big">
-             Brian Windhorst's update on Kevin Durant's trade demands & the Brooklyn Nets' future | Get Up
+            Trevon Diggs on His Critics: ‘They Don’t Know What They’re Talking About’
             </h1>
+            <div className="author-img">
+                <img src="https://images.complex.com/complex/image/upload/t_author_pic/Df3AZWSUYAAIfJx_q7ygok.jpg" alt="Kameron Hay"/>
+            </div>
+            <div className="author-name">
+                <span class="type">By Kameron Hay</span>
+            </div>
+            <div className="author-bio">
+                <p>
+                    Just a writer from St. Louis/Twitter/
+                    <a href="https://twitter.com/Kameron_Hay">@Kameron_Hay
+                    </a>
+                </p>
+            </div>
+            <time class="datetime">Aug 10,2022</time>
+            <div className="header-img">
+                <img src="https://images.complex.com/complex/images/c_crop,h_2214,w_3936,x_0,y_60/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/adu8d5e9lq7ws074npcv/trevon-diggs-nfl?fimg-ssr-default" alt="trevon diggs"/>
+            </div>
+            <figcaption class="header-caption">Image via Getty/Cooper Neill</figcaption>
+            <div className="article-body-main">
+                <div className="article-copy clearfix">
+                    <p>
+                    Playing for the Dallas Cowboys comes with heightened expectations, being under the microscope of the national media and drawing the ire of opposing fans due to how large the brand of “America’s Team” is. For Pro Bowl cornerback Trevon Diggs, that pressure doesn’t faze him.&nbsp;
+                    </p>
+                    <p>
+                    Diggs put together a dominant 2021 campaign that saw him record 11 interceptions, the most in a single season since Champ Bailey tallied 10 in the 2006 NFL season. With a historic season in 2021 comes more eyes in 2022 and people are ready to see whether he can duplicate a record-setting year. Even with his numbers in 2021, there was some criticism for Diggs on his ability to cover but Diggs will not even entertain that banter.
+                    </p>
+                    <p>
+                    “I don’t pay them any attention,” Diggs says. “They don’t know what they are talking about. Literally, what are you talking about?”
+                    </p>
+                    <p>
+                    Regardless of the outcome this season, the Cowboys crown jewel of their secondary appears ready for whatever comes his way.&nbsp;He is trusting the work and preparation in the offseason will continue to push him up the list of elite cornerbacks in the NFL. In an interview with Complex for his recent collaboration with LIDS, Diggs discussed his success in the league thus far, tuning out the noise of his naysayers, his relationship with All-Pro sibling Stefon Diggs, as well as what he looks for in a dominant cornerback and more.
+                    </p>
+                </div>
+            </div>
+        <section>
+            
+        </section>
+        <div className="header-img">
+                <img src="https://images.complex.com/complex/images/c_crop,h_1688,w_3000,x_0,y_0/c_fill,dpr_2.0,f_auto,q_auto,w_680/fl_lossy,pg_1/dcebuymtsifnud0ky3x6/trevon-diggs-interception?fimg-client" alt="catchshot"/>
+        </div>
+            <figcaption class="header-caption">Image via Getty/Cooper Neill</figcaption>
+            <div className="article-body-main">
+                <div className="article-copy clearfix">
+                    <p>
+                    
+                    </p>
+                    <p>
+                    
+                    </p>
+                    <p>
+                    
+                    </p>
+                    <p>
+                    
+                    </p>
+                </div>
+            </div>
+        
+            
         <video loop autoPlay>
         <source
           url=""
@@ -23,18 +92,41 @@ const SportsPage = () => {
         </div>
        </div>
     </section> 
+        <p>
+        <em>(This interview has been edited and condensed for clarity.)</em>
+        </p>
+        <p>
+
+        </p>
+        <section class="section">
+      <div class = "box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+             Brian Windhorst's update on Kevin Durant's trade demands & the Brooklyn Nets' future | Get Up
+            </h1>
+            <YouTube videoId="yT8czc1SvYY" opts={opts} onReady={this._onReady} />
+        </div>
+       </div>
+    </section>
             <section class="section">
                 <div class="box-main">
                     <div class="secondHalf">
                         <h1 class="text-big" id="program">
-                        Clippers are CLEARLY the best in the West with a healthy Kawhi Leonard and Paul George?! 
+                        Clippers are CLEARLY the best in the West with a healthy Kawhi Leonard and Paul George?!
                         </h1>
                     </div>
                 </div>
             </section>
               <section className="section">
                 <div className="box-main">
-                  <div></div>  
+                  <div></div>
+                </div>
+              </section>
+              <section className="section">
+                <div className="box-main">
+                  <div className="thirdHalf">
+                    <h1>Complex Shows</h1>
+                  </div>  
                 </div>
 
               </section>
