ab39c8a088c304849cda5d92a67137066ba577aa
Micah Norrington <manorrington@gmail.com>
2022-08-10 15:22:55 -0400

    pushing shop database changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Testing from "./components/test-component";
+import SportsPage from './pages/SportsPage';
 
 function App() {
   return (
     <div className="App">
       <p>hello world</p>
       <Testing />
+      <SportsPage/>
     </div>
+    
   );
 }
 
