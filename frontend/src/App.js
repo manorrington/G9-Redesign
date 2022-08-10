14ca5fccead4997c0bb670a1e481707ea11457f5
Micah Norrington <manorrington@gmail.com>
2022-08-10 19:37:33 -0400

    displaying data from mongoDB to shop page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import SportsPage from "./pages/SportsPage";
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/sports" element={<SportsPage />} />
+          <Route exact path="/shop" element={<Shop />} />
         </Routes>
         <Footer />
       </Router>
